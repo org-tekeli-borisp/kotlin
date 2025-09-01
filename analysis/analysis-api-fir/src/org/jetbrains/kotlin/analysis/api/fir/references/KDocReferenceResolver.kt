dd7eae7f2549c0fcbaec286de1766ace547027dd
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-09-01 22:52:10 +0200

    [Analysis API] Introduce an internal project service to track the current KDoc resolution strategy
    ^KT-76607

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KDocReferenceResolver.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KDocReferenceResolver.kt
@@ -30,10 +30,12 @@ import org.jetbrains.kotlin.name.isOneSegmentFQN
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.psi.psiUtil.isPropertyParameter
+import org.jetbrains.kotlin.references.utils.KotlinKDocResolutionStrategyProviderService
 import org.jetbrains.kotlin.utils.addIfNotNull
 import org.jetbrains.kotlin.utils.addToStdlib.ifNotEmpty
 import org.jetbrains.kotlin.utils.yieldIfNotNull
 
+@Suppress("DuplicatedCode")
 internal object KDocReferenceResolver {
     /**
      * [symbol] is the symbol referenced by this resolve result.
@@ -76,6 +78,18 @@ internal object KDocReferenceResolver {
         containedTagSectionIfSubject: KDocKnownTag?
     ): Set<KaSymbol> {
         with(analysisSession) {
+            if (KotlinKDocResolutionStrategyProviderService
+                    .getService(useSiteModule.project)
+                    ?.shouldUseExperimentalStrategy() != true
+            ) {
+                return ClassicKDocReferenceResolver.resolveKdocFqName(
+                    analysisSession,
+                    selectedFqName,
+                    fullFqName,
+                    contextElement
+                ).toSet()
+            }
+
             val contextDeclarationOrSelf = contextElement.getContextElementOrSelf()
             val fullSymbolsResolved =
                 resolveKdocFqName(fullFqName, contextDeclarationOrSelf, containedTagSectionIfSubject)

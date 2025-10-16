e65a27a163709bd1febcef488a50c72c90ff2964
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-10-16 21:42:50 +0200

    re-generate tests

--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenSingleModuleInteropTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenSingleModuleInteropTestGenerated.java
@@ -392,6 +392,12 @@ public class FirWasmJsCodegenSingleModuleInteropTestGenerated extends AbstractFi
   }
 
   @Test
+  @TestMetadata("jsReferenceCasts.kt")
+  public void testJsReferenceCasts() {
+    runTest("compiler/testData/codegen/boxWasmJsInterop/jsReferenceCasts.kt");
+  }
+
+  @Test
   @TestMetadata("jsToKotlinAdapters.kt")
   public void testJsToKotlinAdapters() {
     runTest("compiler/testData/codegen/boxWasmJsInterop/jsToKotlinAdapters.kt");

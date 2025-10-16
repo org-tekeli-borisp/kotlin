cd8e05afe43f12bc0f0b6d0f5d4beafee351cab1
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-10-16 11:40:10 +0200

    fix: support "JsAny is C" checks as well

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmTypeOperatorLowering.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmTypeOperatorLowering.kt
@@ -384,10 +384,11 @@ class WasmBaseTypeOperatorTransformer(val context: WasmBackendContext) : IrEleme
 
         val fromType = argument.type
         if (isExternalType(fromType) != isExternalType(toType)) {
-            if (fromType.classifierOrNull == symbols.jsRelatedSymbols.jsReferenceClass) {
-                // special case: JsReference<C> can be implicitly converted to Any and than treated as C.
+            if (fromType.classifierOrNull == symbols.jsRelatedSymbols.jsReferenceClass || fromType == symbols.jsRelatedSymbols.jsAnyType) {
+                // special case: JsReference<C> can be implicitly converted to Any and then treated as C.
                 // On another hand, it can be unsafely cast from another external type, so do not
                 // resolve it to constants even for `JsReference<C> is C` checks.
+                // JsAny is included as it can contain actual JsReference objects.
                 val argumentAsAny = narrowType(fromType, context.irBuiltIns.anyType, argument)
                 return generateIsSubClassTest(argumentAsAny, toType)
             }
--- compiler/testData/codegen/boxWasmJsInterop/jsReferenceCasts.kt
+++ compiler/testData/codegen/boxWasmJsInterop/jsReferenceCasts.kt
@@ -5,15 +5,65 @@
 
 class C(val x: Int)
 
+val c = C(1)
+
+fun testTypeOperations(obj: JsReference<C>) : String? {
+    if (obj !is Any)
+        return "!is Any"
+    if (obj !is C)
+        return "!is C"
+    if (obj is String)
+        return "is String"
+    if (obj is JsString)
+        return "is JsString"
+    if (obj != c)
+        return "!= C"
+//    if (obj !== c)
+//        return "!== C"
+    return null
+}
+
+// same checks as above, but with another formal type of the object
+// unfortunately, we have to duplicate the code as external types cannot be used as reified type arguments
+fun testTypeOperations(obj: JsReference<String>) : String? {
+    if (obj !is Any)
+        return "!is Any"
+    if (obj !is C)
+        return "!is C"
+    if (obj is String)
+        return "is String"
+    if (obj is JsString)
+        return "is JsString"
+    if (obj != c)
+        return "!= C"
+//    if (obj !== c)
+//        return "!== C"
+    return null
+}
+
+fun testTypeOperations(obj: JsAny) : String? {
+    if (obj !is Any)
+        return "!is Any"
+    if (obj !is C)
+        return "!is C"
+    if (obj is JsString)
+        return "is JsString"
+    if (obj != c)
+        return "!= C"
+//    if (obj !== c)
+//        return "!== C"
+    return null
+}
+
 fun box(): String {
-    val c = C(1)
     val jsReference: JsReference<C> = c.toJsReference()
-    if (jsReference !is Any)
-        return "Fail: JsReference !is Any"
-    // currenty forbidden with IMPOSSIBLE_IS_CHECK_ERROR
-    // TODO: fix here or in the shared-prototype branch
-    if (jsReference !is C)
-        return "Fail: JsReference<C> !is C"
+    testTypeOperations(jsReference)?.let { return "Fail: JsReference<C> $it"}
+
+    val jsReferenceWithIncorrectType: JsReference<String> = jsReference.unsafeCast<JsReference<String>>()
+    testTypeOperations(jsReferenceWithIncorrectType)?.let { return "Fail: JsReference<C>.unsafeCast<JsReference<String>>() $it"}
+
+    val jsReferenceAsJsAny: JsAny = jsReference
+    testTypeOperations(jsReferenceAsJsAny)?.let { return "Fail: (JsReference<C> as JsAny) $it"}
 
     val c2 : C = jsReference as Any as C
     if (c !== c2)

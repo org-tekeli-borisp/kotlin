4ed304d2ac547f2824484ad86cd80679091216de
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-10-17 14:48:02 +0200

    [IR] Rewrite common lowerings to use new API for phase creation
    #KT-67460
    #KT-67461
    #KT-67462

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -6,6 +6,8 @@
 package org.jetbrains.kotlin.ir.backend.js
 
 import org.jetbrains.kotlin.backend.common.LoweringContext
+import org.jetbrains.kotlin.backend.common.ModuleLoweringPass
+import org.jetbrains.kotlin.backend.common.PreSerializationLoweringContext
 import org.jetbrains.kotlin.backend.common.ir.PreSerializationSymbols
 import org.jetbrains.kotlin.backend.common.lower.*
 import org.jetbrains.kotlin.backend.common.lower.coroutines.AddContinuationToLocalSuspendFunctionsLowering
@@ -31,6 +33,7 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.inline.*
 import org.jetbrains.kotlin.ir.interpreter.IrInterpreterConfiguration
 import org.jetbrains.kotlin.platform.js.JsPlatforms
+import kotlin.collections.plusAssign
 
 private val validateIrBeforeLowering = makeIrModulePhase(
     ::KlibIrValidationBeforeLoweringPhase,
@@ -145,6 +148,13 @@ private val jsCodeOutliningPhaseOnSecondStage = makeIrModulePhase(
     { context: JsIrBackendContext -> JsCodeOutliningLowering(context) },
     name = "JsCodeOutliningLoweringOnSecondStage",
 )
+
+private fun createJsCodeOutliningPhaseOnFirstStage(context: JsPreSerializationLoweringContext): JsCodeOutliningLowering {
+    return JsCodeOutliningLowering(context) { jsCall, valueDeclaration, container ->
+        context.diagnosticReporter.at(jsCall, container)
+            .report(JsKlibErrors.JS_CODE_CAPTURES_INLINABLE_FUNCTION, valueDeclaration)
+    }
+}
 
 private val jsCodeOutliningPhaseOnFirstStage = makeIrModulePhase(
     { context: JsPreSerializationLoweringContext ->
@@ -295,6 +305,16 @@ private val enumEntryRemovalLoweringPhase = makeIrModulePhase(
     name = "EnumEntryRemovalLowering",
     prerequisite = setOf(enumUsageLoweringPhase)
 )
+
+private fun createUpgradeCallableReferences(context: LoweringContext): UpgradeCallableReferences {
+    return UpgradeCallableReferences(
+        context,
+        upgradeFunctionReferencesAndLambdas = true,
+        upgradePropertyReferences = true,
+        upgradeLocalDelegatedPropertyReferences = true,
+        upgradeSamConversions = false,
+    )
+}
 
 private val upgradeCallableReferences = makeIrModulePhase(
     { ctx: LoweringContext ->
@@ -725,14 +745,17 @@ val mainFunctionCallWrapperLowering = makeIrModulePhase<JsIrBackendContext>(
 
 fun jsLoweringsOfTheFirstPhase(
     languageVersionSettings: LanguageVersionSettings,
-): List<NamedCompilerPhase<JsPreSerializationLoweringContext, IrModuleFragment, IrModuleFragment>> = buildList {
-    if (languageVersionSettings.supportsFeature(LanguageFeature.IrRichCallableReferencesInKlibs)) {
-        this += upgradeCallableReferences
+): List<NamedCompilerPhase<JsPreSerializationLoweringContext, IrModuleFragment, IrModuleFragment>> {
+    val phases = buildList<(JsPreSerializationLoweringContext) -> ModuleLoweringPass> {
+        if (languageVersionSettings.supportsFeature(LanguageFeature.IrRichCallableReferencesInKlibs)) {
+            this += ::createUpgradeCallableReferences
+        }
+        if (languageVersionSettings.supportsFeature(LanguageFeature.IrIntraModuleInlinerBeforeKlibSerialization)) {
+            this += ::createJsCodeOutliningPhaseOnFirstStage
+        }
+        this += loweringsOfTheFirstPhase(languageVersionSettings)
     }
-    if (languageVersionSettings.supportsFeature(LanguageFeature.IrIntraModuleInlinerBeforeKlibSerialization)) {
-        this += jsCodeOutliningPhaseOnFirstStage
-    }
-    this += loweringsOfTheFirstPhase(languageVersionSettings)
+    return createModulePhases(*phases.toTypedArray())
 }
 
 fun getJsLowerings(

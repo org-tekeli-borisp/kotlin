6a9bdee7026351f865fc9b247a6c1dc3f4039142
Artem Kobzar <artem.kobzar@jetbrains.com>
2025-10-28 10:15:30 +0100

    fixup! Simplify intrinsics and function hierarchy

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsSymbols.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsSymbols.kt
@@ -79,7 +79,6 @@ class JsSymbols(
     val coroutineEmptyContinuation: IrPropertySymbol = CallableIds.EmptyContinuation.propertySymbol()
 
     override val returnIfSuspended = CallableIds.returnIfSuspended.functionSymbol()
-    val suspendCoroutineUninterceptedOrReturnJS = CallableIds.suspendCoroutineUninterceptedOrReturnJS.functionSymbol()
 
     override val functionAdapter = ClassIds.FunctionAdapter.classSymbol()
 
@@ -541,7 +540,6 @@ private object CallableIds {
     // JS functions
     val getContinuation = "getContinuation".jsCallableId
     val returnIfSuspended = "returnIfSuspended".jsCallableId
-    val suspendCoroutineUninterceptedOrReturnJS = "suspendCoroutineUninterceptedOrReturnJS".jsCallableId
 
     val jsIsEs6 = "jsIsEs6".jsCallableId
     val VOID = "VOID".jsCallableId

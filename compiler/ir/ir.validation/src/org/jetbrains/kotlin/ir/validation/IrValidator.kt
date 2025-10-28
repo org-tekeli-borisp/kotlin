14b0a01471b7c03f6752ea0bfa8f544bfa4af098
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-10-28 20:47:27 +0100

    Minor. Organize imports

--- compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/IrValidator.kt
+++ compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/IrValidator.kt
@@ -11,12 +11,9 @@ import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.config.IrVerificationMode
 import org.jetbrains.kotlin.ir.IrBuiltIns
 import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationBase
-import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.declarations.IrReplSnippet
 import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
 import org.jetbrains.kotlin.ir.types.IrType

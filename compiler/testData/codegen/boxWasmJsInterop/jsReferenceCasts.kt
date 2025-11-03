75497a42be3e8a616ede0198fac035a92fa9b13c
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-11-03 10:31:24 +0100

    review fix: add test for JsReference to external type

--- compiler/testData/codegen/boxWasmJsInterop/jsReferenceCasts.kt
+++ compiler/testData/codegen/boxWasmJsInterop/jsReferenceCasts.kt
@@ -1,6 +1,21 @@
 // TARGET_BACKEND: WASM
 // MODULE: main
 
+// FILE: jsType.js
+
+class MyJsClass {
+    constructor() {
+        this.value = 41;
+    }
+
+    // Increment the counter by 1 (or a custom step)
+    increment() {
+        this.value += 1;
+        return this.value;
+    }
+}
+
+
 // FILE: jsReferenceCasts.kt
 
 class C(val x: Int)
@@ -49,6 +64,26 @@ fun testTypeOperations(obj: JsAny) : String? {
     return null
 }
 
+external class MyJsClass() {
+    fun increment(): Int
+}
+
+fun createExtTypeInstance(): MyJsClass = js("new MyJsClass()")
+
+fun testJsReferenceToExtType(): String? {
+    val extInstance: MyJsClass = createExtTypeInstance()    // externref
+    val extInstanceAsAny: Any = extInstance                 // JsExternalBox(extInstance)
+    val jsReferenceToExtInstanceAsAny: JsReference<Any> = extInstanceAsAny.toJsReference() // JsExternalBox(extInstance) as externref
+    val a: Any = jsReferenceToExtInstanceAsAny // JsExternalBox(extInstance)
+    val b = a as MyJsClass // unwraps && checks external type
+    if (extInstance !== b) return "Fail: extInstance !== b"
+    if (a !== b) return "Fail: a !== b"
+
+    val result = a.increment() // smart casted
+    if (result != 42) return "Fail: $result != 42"
+    return null
+}
+
 fun box(): String {
     val jsReference: JsReference<C> = c.toJsReference()
     testTypeOperations(jsReference)?.let { return "Fail: JsReference<C> $it"}
@@ -66,6 +101,8 @@ fun box(): String {
     val c3 : C = jsReference as C
     if (c !== c3)
         return "Fail: implicit restoration of JsReference<C>->C shall result in the original object"
+
+    testJsReferenceToExtType()?.let { return it }
 
     return "OK"
 }

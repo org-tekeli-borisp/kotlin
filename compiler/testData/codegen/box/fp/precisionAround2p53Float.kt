f2f0545aa5a438c1bf6b84058de514ed63c58a30
Alevtina.Gamzikova <alevtina.gamzikova@jetbrains.com>
2025-11-03 17:45:53 +0100

    [Tests] Add additional test data for KT-65234

---
+++ compiler/testData/codegen/box/fp/precisionAround2p53Float.kt
@@ -0,0 +1,14 @@
+// WITH_STDLIB
+
+fun box(): String {
+    val p = 1 shl 24
+    val big = p.toFloat()
+
+    val t1 = (big + 1.0f) - big
+    if (t1 != 0.0f) return "fail1"
+
+    val t2 = big - (big - 1.0f)
+    if (t2 != 1.0f) return "fail2"
+
+    return "OK"
+}

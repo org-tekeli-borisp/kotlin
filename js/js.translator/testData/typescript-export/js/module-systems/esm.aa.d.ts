b65e003c553a8f7575fba2f41f93a9164a1980e6
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-11-04 18:28:50 +0100

    [test] Test infra support for Analysis API-based TypeScript export
    Currently, box tests only, support for invalidation tests is coming
    later.
    
    KT-80865

---
+++ js/js.translator/testData/typescript-export/js/module-systems/esm.aa.d.ts
@@ -0,0 +1,14 @@
+type Nullable<T> = T | null | undefined
+declare function KtSingleton<T>(): T & (abstract new() => any);
+/* ErrorDeclaration: Class declarations are not implemented yet */
+/* ErrorDeclaration: Class declarations are not implemented yet */
+
+/* ErrorDeclaration: Top level property declarations are not implemented yet */
+/* ErrorDeclaration: Top level property declarations are not implemented yet */
+/* ErrorDeclaration: Top level function declarations are not implemented yet */
+/* ErrorDeclaration: Top level function declarations are not implemented yet */
+/* ErrorDeclaration: Top level function declarations are not implemented yet */
+/* ErrorDeclaration: Class declarations are not implemented yet */
+/* ErrorDeclaration: Class declarations are not implemented yet */
+/* ErrorDeclaration: Class declarations are not implemented yet */
+/* ErrorDeclaration: Class declarations are not implemented yet */

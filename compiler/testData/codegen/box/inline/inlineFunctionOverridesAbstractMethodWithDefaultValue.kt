50eb01b5d1e3622fc037ae40c7ec65d11f6cbc76
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-11-04 11:10:23 +0200

    fixup! [Tests] KT-82017: inline function overrides method with default value

--- compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValue.kt
+++ compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValue.kt
@@ -1,6 +1,6 @@
 // ISSUE: KT-82017
 // LANGUAGE: -IrIntraModuleInlinerBeforeKlibSerialization -IrCrossModuleInlinerBeforeKlibSerialization
-// IGNORE_BACKEND: JS_IR, WASM_JS, WASM_WASI, NATIVE, JVM_IR
+// IGNORE_BACKEND: JS_IR, JS_IR_ES6, WASM_JS, WASM_WASI, NATIVE, JVM_IR
 interface I {
     abstract fun foo(a: String = "FAIL"): String
 }

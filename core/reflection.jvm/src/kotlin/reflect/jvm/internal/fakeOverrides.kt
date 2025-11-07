f89e0f48b1848d84a1f4bb2844b75d1e7d445297
Nikita Bobko <nikita.bobko@jetbrains.com>
2025-11-07 19:33:02 +0100

    wip | 2025-11-07T18:33:02Z

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/fakeOverrides.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/fakeOverrides.kt
@@ -16,6 +16,7 @@ import kotlin.reflect.jvm.internal.types.KTypeSubstitutor
 import kotlin.reflect.jvm.internal.types.MutableCollectionKClass
 import kotlin.reflect.jvm.internal.types.ReflectTypeSystemContext
 import kotlin.reflect.jvm.javaField
+import kotlin.reflect.jvm.javaMethod
 
 /**
  * Unfortunately, getting members is not a transitive operation
@@ -23,8 +24,9 @@ import kotlin.reflect.jvm.javaField
 internal fun getAllMembers_newKotlinReflectImpl(kClass: KClassImpl<*>): Collection<DescriptorKCallable<*>> {
     val membersReadOnly = kClass.data.value.allMembersPreservingTransitivity
     // Kotlin doesn't have statics (unless it's enum), and it never inherits statics from Java
+    val isKotlin = kClass.java.isKotlin
     val doNeedToFilterOutStatics =
-        membersReadOnly.containsInheritedStatics && kClass.classKind != ClassKind.ENUM_CLASS && kClass.java.isKotlin
+        membersReadOnly.containsInheritedStatics && kClass.classKind != ClassKind.ENUM_CLASS && isKotlin
     val doNeedToShrinkMembers = membersReadOnly.containsPackagePrivate || doNeedToFilterOutStatics
     val membersMutable = when (doNeedToShrinkMembers) {
         true -> lazyOf(
@@ -37,19 +39,16 @@ internal fun getAllMembers_newKotlinReflectImpl(kClass: KClassImpl<*>): Collecti
     }
     // Populating the 'members' list with things which are not inherited but appear in the 'members' list
     for (declaredMember in kClass.declaredDescriptorKCallableMembers) {
-        val signature = declaredMember.toEquatableCallableSignature()
-        // static members are not inherited, but the immediate statics in interfaces must appear in the 'members' list
-        if (declaredMember.isStatic && kClass.classKind == ClassKind.INTERFACE) {
+        val signature = declaredMember.toEquatableCallableSignature(isKotlin)
+        if (// static members are not inherited, but the immediate statics in interfaces must appear in the 'members' list
+            declaredMember.isStatic && kClass.classKind == ClassKind.INTERFACE ||
+
+            // private members are not inherited, but immediate private members must appear in the 'members' list
+            declaredMember.visibility == KVisibility.PRIVATE
+        ) {
             membersMutable.value[signature] = declaredMember
         }
-        // private members are not inherited, but immediate private members must appear in the 'members' list
-        if (declaredMember.visibility == KVisibility.PRIVATE) membersMutable.value[signature] = declaredMember
     }
-    // val foo = membersReadOnly.allMembers.values.first { it.name == "bar" }
-    // foo.shallowCopy().apply {
-    //     kTypeSubstitutor = foo.kTypeSubstitutor
-    //     forceInstanceReceiverParameter = foo.forceInstanceReceiverParameter
-    // }.toString()
     return (if (membersMutable.isInitialized()) membersMutable.value else membersReadOnly.allMembers)
         .map { it.value }
 }
@@ -61,7 +60,8 @@ private fun starProjectionSupertypesAreNotPossible(): Nothing = error("Star proj
 private object CovariantOverrideComparator : Comparator<DescriptorKCallable<*>> {
     override fun compare(a: DescriptorKCallable<*>, b: DescriptorKCallable<*>): Int {
         val typeParametersEliminator = b.typeParameters.substituteTypeParametersInto(a.typeParameters)
-        val aReturnType = typeParametersEliminator.substitute(a.returnType).type ?: starProjectionSupertypesAreNotPossible()
+        val aReturnType =
+            typeParametersEliminator.substitute(a.returnType).type ?: starProjectionSupertypesAreNotPossible()
         val bReturnType = b.returnType
         val aIsSubtypeOfB = aReturnType.isSubtypeOf(bReturnType)
         val bIsSubtypeOfA = bReturnType.isSubtypeOf(aReturnType)
@@ -104,14 +104,30 @@ private fun KType.coerceMutableCollectionTypeToImmutableCollectionType(): KType 
     else -> this
 }
 
+private fun collectDeclaredMembersToMap(map: HashMap<EquatableCallableSignature, DescriptorKCallable<*>>) {
+
+}
+
 /**
  * todo KDoc
  */
 internal fun getAllMembersPreservingTransitivity(kClass: KClassImpl<*>): AllMembersPreservingTransitivity {
-    val result = HashMap<EquatableCallableSignature, DescriptorKCallable<*>>()
+    val jvmSignaturesMap = HashMap<EquatableCallableSignature, DescriptorKCallable<*>>()
     val thisReceiver = kClass.descriptor.thisAsReceiverParameter
     var containsInheritedStatics = false
     var containsPackagePrivate = false
+    val isKotlin = kClass.java.isKotlin
+    val declaredMembersSignature = if (isKotlin) EqualityMode.KotlinSignatures else EqualityMode.JvmSignatures
+    val declaredMembersSignaturesMap = HashMap<EquatableCallableSignature, DescriptorKCallable<*>>()
+    for (member in kClass.declaredDescriptorKCallableMembers) {
+        if (member.visibility == KVisibility.PRIVATE) continue
+        val isStaticMember = member.isStatic
+        // static methods (but not fields) in interfaces are never inherited (not in Java, not in Kotlin enums)
+        if (isStaticMember && kClass.classKind == ClassKind.INTERFACE && !member.isJavaField) continue
+        containsInheritedStatics = containsInheritedStatics || isStaticMember
+        containsPackagePrivate = containsPackagePrivate || member.isPackagePrivate
+        declaredMembersSignaturesMap[member.toEquatableCallableSignature(declaredMembersSignature)] = member
+    }
     for (rawSupertype in kClass.supertypes) {
         // Because mutable collections are severely broken in reflect. Related: KT-11754
         val supertype = rawSupertype.coerceMutableCollectionTypeToImmutableCollectionType()
@@ -125,21 +141,18 @@ internal fun getAllMembersPreservingTransitivity(kClass: KClassImpl<*>): AllMemb
                 forceInstanceReceiverParameter = if (notSubstitutedMember.isStatic) null else thisReceiver
                 kTypeSubstitutor = notSubstitutedMember.kTypeSubstitutor.combinedWith(substitutor)
             }
-            result.merge(member.toEquatableCallableSignature(), member) { a, b ->
+            val kotlinSignature = member.toEquatableCallableSignature(declaredMembersSignature)
+            if (declaredMembersSignaturesMap.contains(kotlinSignature)) continue
+            // Inherited signatures are always compared by JvmSignatures
+            jvmSignaturesMap.merge(kotlinSignature.copy(equalityMode = EqualityMode.JvmSignatures), member) { a, b ->
                 minOf(a, b, CovariantOverrideComparator)
             }
         }
     }
-    for (member in kClass.declaredDescriptorKCallableMembers) {
-        if (member.visibility == KVisibility.PRIVATE) continue
-        val isStaticMember = member.isStatic
-        // static methods (but not fields) in interfaces are never inherited (not in Java, not in Kotlin enums)
-        if (isStaticMember && kClass.classKind == ClassKind.INTERFACE && !member.isJavaField) continue
-        containsInheritedStatics = containsInheritedStatics || isStaticMember
-        containsPackagePrivate = containsPackagePrivate || member.isPackagePrivate
-        result[member.toEquatableCallableSignature()] = member
+    for ((kotlinSignature, descriptor) in declaredMembersSignaturesMap) {
+        jvmSignaturesMap[kotlinSignature.copy(equalityMode = EqualityMode.JvmSignatures)] = descriptor
     }
-    return AllMembersPreservingTransitivity(result, containsInheritedStatics, containsPackagePrivate)
+    return AllMembersPreservingTransitivity(jvmSignaturesMap, containsInheritedStatics, containsPackagePrivate)
 }
 
 internal val DescriptorKCallable<*>.isStatic: Boolean
@@ -155,7 +168,7 @@ private val KClass<*>.allMembersPreservingTransitivity: AllMembersPreservingTran
         else -> error("Unknown type ${this::class}")
     }
 
-private fun DescriptorKCallable<*>.toEquatableCallableSignature(): EquatableCallableSignature {
+private fun DescriptorKCallable<*>.toEquatableCallableSignature(equalityMode: EqualityMode): EquatableCallableSignature {
     val parameterTypes = parameters.filter { it.kind != KParameter.Kind.INSTANCE }.map { it.type }
     val kind = when (this) {
         is KProperty<*> if javaField?.declaringClass?.isKotlin == false -> SignatureKind.FIELD_IN_JAVA_CLASS
@@ -163,7 +176,16 @@ private fun DescriptorKCallable<*>.toEquatableCallableSignature(): EquatableCall
         is KFunction<*> -> SignatureKind.FUNCTION
         else -> error("Unknown kind for ${this::class}")
     }
-    return EquatableCallableSignature(kind, name, this.typeParameters, parameterTypes, isStatic)
+    val javaParameterTypes = (this as? KFunction<*>)?.javaMethod?.parameterTypes.orEmpty().toList()
+    return EquatableCallableSignature(
+        kind,
+        name,
+        this.typeParameters,
+        parameterTypes,
+        javaParameterTypes,
+        isStatic,
+        equalityMode,
+    )
 }
 
 internal enum class SignatureKind {
@@ -184,33 +206,49 @@ private fun List<KTypeParameter>.substituteTypeParametersInto(typeParameters: Li
     return KTypeSubstitutor(substitutionMap)
 }
 
+enum class EqualityMode {
+    KotlinSignatures, JvmSignatures
+}
+
 // Signatures that you can test for equality
 internal data class EquatableCallableSignature(
     val kind: SignatureKind,
     val name: String,
     val typeParameters: List<KTypeParameter>,
     val parameterTypes: List<KType>,
+    val javaParameterTypesIfFunction: List<Class<*>>,
     val isStatic: Boolean,
+    val equalityMode: EqualityMode,
 ) {
     init {
         check(kind != SignatureKind.FIELD_IN_JAVA_CLASS || parameterTypes.isEmpty() && typeParameters.isEmpty())
     }
 
-    override fun hashCode(): Int = Objects.hash(kind, name, typeParameters.size, parameterTypes.size, isStatic)
+    override fun hashCode(): Int = Objects.hash(kind, name, parameterTypes.size, isStatic)
 
     override fun equals(other: Any?): Boolean {
         if (other !is EquatableCallableSignature) return false
+        check(equalityMode == other.equalityMode) {
+            "Equality modes must be the same. Please recreate signatures on inheritance"
+        }
         if (kind != other.kind) return false
         if (name != other.name) return false
         if (isStatic != other.isStatic) return false
-        if (typeParameters.size != other.typeParameters.size) return false
         if (parameterTypes.size != other.parameterTypes.size) return false
-        val functionTypeParametersEliminator = other.typeParameters.substituteTypeParametersInto(typeParameters)
-        for (i in parameterTypes.indices) {
-            val x = functionTypeParametersEliminator.substitute(parameterTypes[i]).type
-                ?: starProjectionSupertypesAreNotPossible()
-            val y = other.parameterTypes[i]
-            if (!x.isSubtypeOf(y) || !y.isSubtypeOf(x)) return false
+        if (equalityMode == EqualityMode.JvmSignatures && kind == SignatureKind.FUNCTION) {
+            if (javaParameterTypesIfFunction.size != other.javaParameterTypesIfFunction.size) return false
+            for (i in javaParameterTypesIfFunction.indices) {
+                if (javaParameterTypesIfFunction[i] != other.javaParameterTypesIfFunction[i]) return false
+            }
+        } else {
+            if (typeParameters.size != other.typeParameters.size) return false
+            val functionTypeParametersEliminator = other.typeParameters.substituteTypeParametersInto(typeParameters)
+            for (i in parameterTypes.indices) {
+                val x = functionTypeParametersEliminator.substitute(parameterTypes[i]).type
+                    ?: starProjectionSupertypesAreNotPossible()
+                val y = other.parameterTypes[i]
+                if (!x.isSubtypeOf(y) || !y.isSubtypeOf(x)) return false
+            }
         }
         return true
     }
